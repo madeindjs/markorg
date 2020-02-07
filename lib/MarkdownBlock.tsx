703c0122330fac5c97140f47159fae7ff18c0523
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-02-07 09:35:02 +0100

    Small fix

--- lib/MarkdownBlock.tsx
+++ lib/MarkdownBlock.tsx
@@ -12,20 +12,20 @@ function getElements(node: Node, index: number): JSX.Element {
 
     switch (node.type) {
         case "bold":
-            var textNode = node.block[0] as TextNode;
-            var content = `${node.style}${textNode.text}${node.style}`;
+            var text = node.block.map((n: TextNode) => n.text).join(' ')
+            var content = `${node.style}${text}${node.style}`;
             return <Strong content={content} key={index} />;
         case "italic":
-            console.log(node.block)
-            var textNode = node.block[0] as TextNode;
-            var content = `${node.style}${textNode.text}${node.style}`;
+            // console.log(node.block)
+            var text = node.block.map((n: TextNode) => n.text).join(' ')
+            var content = `${node.style}${text}${node.style}`;
             return <Italic content={content} key={index} />;
         case "codeSpan":
             var content = `\`${node.code}\``;
             return <Italic content={content} key={index} />;
         case "strike":
-            var textNode = node.block[0] as TextNode;
-            var content = `${node.style}${textNode.text}${node.style}`;
+            var text = node.block.map((n: TextNode) => n.text).join(' ')
+            var content = `${node.style}${text}${node.style}`;
             return <Strike content={content} key={index} />;
         case "text":
             return <Text key={index}>{node.text}</Text>;
