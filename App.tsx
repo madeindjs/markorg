0ff08fc11fd30ca4da1e99362ba1c0ea138bfa3b
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2020-03-05 11:22:03 +0100

    Improve naming

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MarkdownEditor from './lib/MarkdownEditor';
+import Editor from './lib/Editor';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <MarkdownEditor />
+            <Editor />
         </View>
     );
 }
